a98de414045eec92bc61eabaf5142e9e287e1315
Gino Ochoa <gochoa1088@gmail.com>
2022-12-02 15:10:00 -0800

    create posts table

---
+++ migrations/20221202225728_create-posts-table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("posts", (table) => {
+    table.increments();
+    table.text("author", 128).defaultTo("Anon");
+    table.text("content");
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
