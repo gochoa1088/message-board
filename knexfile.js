7da2ae6049fff38a3f85a9e20107c49626cd6ccc
gochoa1088 <gochoa1088@gmail.com>
2022-12-07 20:42:03 -0800

    user/pw env

--- knexfile.js
+++ knexfile.js
@@ -12,28 +12,12 @@ module.exports = {
     useNullAsDefault: true,
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-
   production: {
     client: "postgresql",
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+      database: "message-board",
+      user: process.env.USERNAME,
+      password: process.env.PASSWORD,
     },
     pool: {
       min: 2,
