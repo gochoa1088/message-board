ac7b18471899f3766d2cded5d96455218b58e2d6
gochoa1088 <gochoa1088@gmail.com>
2022-12-07 16:41:36 -0800

    migrations: change 'blog_id' to 'id' and add 'users' table

---
+++ migrations/20221208003719_users-table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id");
+    table.text("author", 128).defaultTo("Anon");
+    table.integer("votes").defaultTo(0);
+    table.timestamps(true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users");
+};
